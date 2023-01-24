bf4023376b8a7f85ce800fa355b6dea712897765
Lucas <lucas_emanuel_sc@hotmail.com>
2023-01-24 18:39:20 -0300

    tirei os log que usei pros erros

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -6,8 +6,6 @@ export default class MatchesController {
   public getMatches = async (req: Request, res: Response): Promise<void> => {
     const { inProgress } = req.query;
     const matches = await this.matchService.getAllMatches();
-    console.log(matches);
-    console.log('dentro da controller', inProgress);
     if (inProgress) {
       const filterMatches = matches.filter((match) => match.inProgress.toString() === inProgress);
       res.status(200).json(filterMatches);
--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -3,18 +3,12 @@ import teamsModel from '../database/models/Teams.Model';
 
 export default class MatchesService {
   public getAllMatches = async (): Promise<matchesModel[]> => {
-    console.log('to no service AAAAAAAAAAAAAAAAAAAA');
-    try {
-      const allMatches = await matchesModel.findAll({
-        include: [
-          { model: teamsModel, as: 'homeTeam', attributes: ['teamName'] },
-          { model: teamsModel, as: 'awayTeam', attributes: ['teamName'] },
-        ],
-      });
-      return allMatches;
-    } catch (e) {
-      console.log(e);
-    }
-    return null as unknown as matchesModel[];
+    const allMatches = await matchesModel.findAll({
+      include: [
+        { model: teamsModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: teamsModel, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
+    return allMatches;
   };
 }
